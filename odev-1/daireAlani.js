b723794bfe7180605c2c2d28e6507ab5abeb2595
yasinbyzli <yasinbeeyazli@gmail.com>
2021-05-04 10:57:33 +0300

    odev-1, virgülden soranki 2 basamak eklendi

--- odev-1/daireAlani.js
+++ odev-1/daireAlani.js
@@ -2,7 +2,7 @@ const arguments = process.argv.slice(2);
 
 const calculate = (radius) => {
     let alan = Math.PI * radius**2;
-    console.log(`Dairenin alanı ${alan}`)
+    console.log(`Dairenin alanı ${alan.toFixed(2)}`)
 }
 
 calculate(arguments[0])
